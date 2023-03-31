9252bdf377dba80ce480c990f232d6263b51f879
Leonidas N. Kalousis <leonidas.kalousis@gmail.com>
2023-03-31 20:42:38 +0300

    Add LinkDef.h

---
+++ LinkDef.h
@@ -0,0 +1,22 @@
+
+#ifdef __CINT__
+ 
+#pragma link off all globals;
+#pragma link off all classes;
+#pragma link off all functions;
+#pragma link C++ nestedclasses;
+#pragma link C++ nestedtypedefs;
+
+#pragma link C++ namespace PMTStyle+;
+#pragma link C++ namespace PMType+;
+
+#pragma link C++ class Pedestal+;
+#pragma link C++ class SPEResponse+;
+#pragma link C++ class PMT+;
+
+#pragma link C++ class DFTmethod+;
+#pragma link C++ class PMTModel+;
+
+#pragma link C++ class SPEFitter+;
+
+#endif
